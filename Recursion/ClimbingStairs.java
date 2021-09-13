847a04204677004e183c3b3e0064b077f0a86b72
Lalit Kaim <kaim.lalit1234@gmail.com>
2021-09-13 09:49:25 +0530

    Add files via upload

---
+++ Recursion/ClimbingStairs.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class ClimbingStairs{
+    public static int rec(int n){
+        if(n==1 || n==0)
+            return 1;
+        return rec(n-1)+rec(n-2);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(rec(n));
+    }
+}
