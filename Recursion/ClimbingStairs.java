92dbfd4848efd60897597e8d2a6195556572b5bc
Lalit Kaim <kaim.lalit1234@gmail.com>
2021-09-13 09:52:04 +0530

    Update ClimbingStairs.java

--- Recursion/ClimbingStairs.java
+++ Recursion/ClimbingStairs.java
@@ -9,6 +9,6 @@ class ClimbingStairs{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println(rec(n));
+        System.out.println("No. of ways to reach nth stair : "+rec(n));
     }
 }
